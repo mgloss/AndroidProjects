d9fdb3d03456b8d4a0677507dd941de69a9d6fb8
mgloss <michaela.glossova@gmail.com>
2017-12-20 12:26:31 +0100

    Improved comments for methods

--- GoalballCounter/app/src/main/java/com/example/android/goalballcounter/MainActivity.java
+++ GoalballCounter/app/src/main/java/com/example/android/goalballcounter/MainActivity.java
@@ -21,21 +21,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
+    /**
+     * Adding a goal to a score for Team A.
+     */
     public void addGoalA(View View) {
         scoreA = scoreA + 1;
         displayForTeamA(scoreA);
     }
+    /**
+     * Adding penalty for Team A.
+     */
     public void addPenaltyA(View View) {
         penaltyA = penaltyA + 1;
         displayPenaltyForTeamA(penaltyA);
     }
+    /**
+     * Adding penalty goal and also add a goal to the total score for Team A.
+     */
     public void addPenaltyGoalA(View View) {
         scoreA = scoreA + 1;
         penaltyGoalA = penaltyGoalA + 1;
         displayForTeamA(scoreA);
         displayPenaltyGoalForTeamA(penaltyGoalA);
     }
-
+    /**
+     * Reset all values.
+     */
     public void resetScore(View View) {
         scoreA = 0;
         scoreB = 0;
@@ -50,14 +61,23 @@ public class MainActivity extends AppCompatActivity {
         displayPenaltyGoalForTeamA(penaltyGoalA);
         displayPenaltyGoalForTeamB(penaltyGoalB);
     }
+    /**
+     * Adding a goal to a score for Team B.
+     */
     public void addGoalB(View View) {
         scoreB = scoreB + 1;
         displayForTeamB(scoreB);
     }
+    /**
+     * Adding penalty for Team A.
+     */
     public void addPenaltyB(View View) {
         penaltyB = penaltyB + 1;
         displayPenaltyForTeamB(penaltyB);
     }
+    /**
+     * Adding penalty goal and also add a goal to the total score for Team B.
+     */
     public void addPenaltyGoalB(View View) {
         scoreB = scoreB + 1;
         penaltyGoalB = penaltyGoalB + 1;
@@ -72,10 +92,16 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+    /**
+     * Displays number of penalty for Team A.
+     */
     public void displayPenaltyForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score_penalty);
         scoreView.setText(String.valueOf(score));
     }
+    /**
+     * Displays number od penalty Goals Team A.
+     */
     public void displayPenaltyGoalForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score_penalty_goal);
         scoreView.setText(String.valueOf(score));
@@ -87,10 +113,16 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
+    /**
+     * Displays number of penalty for Team A.
+     */
     public void displayPenaltyForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score_penalty);
         scoreView.setText(String.valueOf(score));
     }
+    /**
+     * Displays number od penalty Goals Team A.
+     */
     public void displayPenaltyGoalForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score_penalty_goal);
         scoreView.setText(String.valueOf(score));
